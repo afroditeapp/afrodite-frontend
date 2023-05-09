0a4ff2fe606946a90c675cada966a6ceb05b4192
Juuso Tuononen <jutuon@outlook.com>
2023-05-09 18:38:40 +0300

    Add some settings

--- lib/ui/normal/settings.dart
+++ lib/ui/normal/settings.dart
@@ -10,8 +10,41 @@ class SettingsView extends StatefulWidget {
 class _SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("Settings"),
+    List<Setting> settings = [
+      createSetting(Icons.account_circle, "My profile", () => {}),
+      createSetting(Icons.admin_panel_settings, "Admin", () => {}),
+    ];
+    return ListView.builder(
+      itemCount: settings.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {
+            print(index);
+            settings[index].action();
+          },
+          title: settings[index].widget,
+        );
+      },
     );
   }
+
+  Setting createSetting(IconData icon, String text, void Function() action) {
+    Widget widget = Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Row(
+        children: [
+          Icon(icon, size: 50,),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+          Text(text),
+        ],
+      ),
+    );
+    return Setting(widget, action);
+  }
+}
+
+class Setting {
+  final Widget widget;
+  final void Function() action;
+  Setting(this.widget, this.action);
 }
