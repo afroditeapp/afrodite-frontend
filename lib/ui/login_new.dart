4cbfa43458ab647f2f0f4402eb7858a34128674c
Juuso Tuononen <jutuon@outlook.com>
2024-05-26 12:32:29 +0300

    Add TODO comment

--- lib/ui/login_new.dart
+++ lib/ui/login_new.dart
@@ -28,6 +28,9 @@ import "package:url_launcher/url_launcher_string.dart";
 // TODO(prod): Use SVG for sign in with google button
 // TODO(prod): Sign in with buttons dark theme support
 
+// TODO(prod): Show progress when sign in with google/apple returns and
+// connecting to server starts
+
 class LoginScreen extends RootScreen {
   const LoginScreen({Key? key}) : super(key: key);
 
