4c1a2b0adff6e1d9032f66e4628984bc54babfec
Juuso Tuononen <jutuon@outlook.com>
2024-06-13 23:41:18 +0300

    Fix notifications on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // https://github.com/MaikuB/flutter_local_notifications/tree/master/flutter_local_notifications#-ios-setup
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
