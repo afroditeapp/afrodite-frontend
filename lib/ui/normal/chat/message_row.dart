bc42bd3b47059de100c92b97fc106b965c35c5bd
Juuso Tuononen <jutuon@outlook.com>
2024-08-13 18:11:29 +0300

    Fix message layout

--- lib/ui/normal/chat/message_row.dart
+++ lib/ui/normal/chat/message_row.dart
@@ -25,7 +25,7 @@ Align messageRowWidget(BuildContext context, MessageViewEntry entry, {Key? key, 
             child: Align(
               key: key,
               alignment: isSent ? Alignment.centerRight : Alignment.centerLeft,
-              child: messageWidget(context, message, sentMessageState, key: key, parentTextStyle: parentTextStyle),
+              child: _messageAndErrorWidget(context, message, sentMessageState, parentTextStyle: parentTextStyle),
             ),
           ),
         ],
@@ -34,35 +34,48 @@ Align messageRowWidget(BuildContext context, MessageViewEntry entry, {Key? key, 
   );
 }
 
-Widget messageWidget(BuildContext context, String message, SentMessageState? sentMessageState, {Key? key, required TextStyle parentTextStyle}) {
+Widget _messageAndErrorWidget(BuildContext context, String message, SentMessageState? sentMessageState, {required TextStyle parentTextStyle}) {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Flexible(
+        flex: 1,
+        child: Visibility(
+          visible: sentMessageState == SentMessageState.sendingError,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          child: Icon(Icons.error, color: Theme.of(context).colorScheme.error),
+        ),
+      ),
+      Flexible(
+        flex: 10,
+        child: _messageWidget(context, message, sentMessageState, parentTextStyle: parentTextStyle),
+      ),
+    ],
+  );
+}
+
+Widget _messageWidget(BuildContext context, String message, SentMessageState? sentMessageState, {required TextStyle parentTextStyle}) {
   final styleChanges = TextStyle(
     // color: Theme.of(context).colorScheme.onPrimary,
     color: Theme.of(context).colorScheme.onPrimaryContainer,
     fontSize: 16.0,
   );
-
   final style = parentTextStyle.merge(
     styleChanges,
   );
-
-  return Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      if (sentMessageState == SentMessageState.sendingError)
-        Icon(Icons.error, color: Theme.of(context).colorScheme.error),
-      Container(
-        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        decoration: BoxDecoration(
-          // color: Theme.of(context).colorScheme.primary,
-          color: Theme.of(context).colorScheme.primaryContainer,
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        child: Text(
-          message,
-          style: style,
-        ),
-      ),
-    ],
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+    padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+    decoration: BoxDecoration(
+      // color: Theme.of(context).colorScheme.primary,
+      color: Theme.of(context).colorScheme.primaryContainer,
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    child: Text(
+      message,
+      style: style,
+    ),
   );
 }
