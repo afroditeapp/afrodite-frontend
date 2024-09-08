4dc965a0a122693b65abab7ea0088c11fe2c154c
Juuso Tuononen <jutuon@outlook.com>
2024-09-08 22:10:02 +0300

    Remove unnecessary config
    Sign in with Google on iOS uses plist file for config.

--- lib/data/login_repository.dart
+++ lib/data/login_repository.dart
@@ -23,7 +23,6 @@ import 'package:pihka_frontend/database/background_database_manager.dart';
 import 'package:pihka_frontend/database/database_manager.dart';
 import 'package:pihka_frontend/logic/app/app_visibility_provider.dart';
 import 'package:pihka_frontend/main.dart';
-import 'package:pihka_frontend/secrets.dart';
 import 'package:utils/utils.dart';
 import 'package:pihka_frontend/utils/result.dart';
 import 'package:rxdart/rxdart.dart';
@@ -511,20 +510,16 @@ enum SignInWithGoogleEvent {
   otherError,
 }
 
-// TODO: make sure that iOS client id does not end in Android apk.
-
 const String emailScope = "https://www.googleapis.com/auth/userinfo.email";
 
 GoogleSignIn createSignInWithGoogle() {
   // TODO(web): Sign in with Google support for web
-  if (kIsWeb || Platform.isAndroid) {
+  if (kIsWeb) {
     return GoogleSignIn(
       scopes: [emailScope],
     );
-  } else if (Platform.isIOS) {
+  } else if (Platform.isIOS || Platform.isAndroid) {
     return GoogleSignIn(
-      clientId: signInWithGoogleIosClientId(),
-      serverClientId: signInWithGoogleBackendClientId(),
       scopes: [emailScope],
     );
   } else {
