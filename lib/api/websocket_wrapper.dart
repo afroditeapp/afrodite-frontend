6bdf487e4101729c21426c505e1019263857ee0c
Juuso Tuononen <jutuon@outlook.com>
2024-09-22 11:19:09 +0300

    Fix missing WebSocket connection ping
    Changing UI to conversation screen reset the timer which caused client
    to not send ping to server and that caused server to timeout the
    connection.
    
    Now the 5 minute timer is always running and the 10 second timer is an
    extra timer. It does not matter if the extra timer sends ping directly
    after the first timer as this happens quite rarely.

--- lib/api/websocket_wrapper.dart
+++ lib/api/websocket_wrapper.dart
@@ -13,8 +13,11 @@ class WebSocketWrapper {
   WebSocketWrapper(this.connection);
 
   bool _closed = false;
-  Duration? _pingInterval;
-  StreamSubscription<void>? _pingTimer;
+
+  Duration? _extraPingTimerInterval;
+  StreamSubscription<void>? _extraPingTimer;
+
+  StreamSubscription<void>? _defaultPingTimer;
 
   Future<void> close() async {
     if (_closed) {
@@ -22,9 +25,12 @@ class WebSocketWrapper {
     }
     _closed = true;
 
-    final p = _pingTimer;
-    _pingTimer = null;
+    final p = _extraPingTimer;
+    final p2 = _defaultPingTimer;
+    _extraPingTimer = null;
+    _defaultPingTimer = null;
     await p?.cancel();
+    await p2?.cancel();
     if (connection.closeCode == null) {
       await connection.sink.close();
     }
@@ -40,22 +46,31 @@ class WebSocketWrapper {
 
     final visiblePage = navigatorState.pages.lastOrNull;
 
-    if (visiblePage != null && visiblePage.pageInfo is ConversationPageInfo && _pingInterval != _CONVERSATION_PING_INTERVAL) {
-      await _resetPingLogic(_CONVERSATION_PING_INTERVAL);
-    } else if (_pingInterval != _DEFAULT_PING_INTERVAL) {
-      await _resetPingLogic(_DEFAULT_PING_INTERVAL);
+    if (visiblePage != null && visiblePage.pageInfo is ConversationPageInfo && _extraPingTimerInterval != _CONVERSATION_PING_INTERVAL) {
+      await _resetPingLogicForExtraTimer(_CONVERSATION_PING_INTERVAL);
+    } else if (_extraPingTimerInterval != null) {
+      await _resetPingLogicForExtraTimer(null);
     }
-  }
 
-  Future<void> _resetPingLogic(Duration interval) async {
-    final p = _pingTimer;
-    _pingTimer = null;
-    await p?.cancel();
-    _pingInterval = interval;
-    _pingTimer = Stream<void>.periodic(interval, (_) {
+    _defaultPingTimer ??= Stream<void>.periodic(_DEFAULT_PING_INTERVAL, (_) {
       if (!_closed && connection.closeCode == null) {
         connection.sink.add(Uint8List(0)); // Server ignores empty binary messages
       }
     }).listen((_) {});
+  }
+
+  /// Null stops the extra timer
+  Future<void> _resetPingLogicForExtraTimer(Duration? interval) async {
+    final p = _extraPingTimer;
+    _extraPingTimer = null;
+    await p?.cancel();
+    _extraPingTimerInterval = interval;
+    if (interval != null) {
+      _extraPingTimer = Stream<void>.periodic(interval, (_) {
+        if (!_closed && connection.closeCode == null) {
+          connection.sink.add(Uint8List(0)); // Server ignores empty binary messages
+        }
+      }).listen((_) {});
+    }
   }
 }
