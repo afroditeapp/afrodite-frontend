54d028a1c8658b315e60babb496519e3557c7a84
Juuso Tuononen <jutuon@outlook.com>
2024-10-01 12:29:53 +0300

    Set default server address for web client development

--- lib/config.dart
+++ lib/config.dart
@@ -5,17 +5,21 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 const String _defaultAccountServerAddressAndroid = "***REMOVED***"; // Should point to 10.0.2.2 (Android emulator host)
-const String _defaultAccountServerAddressIos = "http://localhost:3000"; // This address is for iOS simulator
+const String _defaultAccountServerAddressIosAndWeb = "http://localhost:3000"; // This address is for iOS simulator and web browsers
 
 const String developmentServerUrl = "***REMOVED***";
 
 String defaultServerUrlAccount() {
+  if (kProfileMode && kIsWeb) {
+    return _defaultAccountServerAddressIosAndWeb;
+  }
+
   if (!kDebugMode) {
      return developmentServerUrl;
   }
 
   if (kIsWeb || Platform.isIOS) {
-    return _defaultAccountServerAddressIos;
+    return _defaultAccountServerAddressIosAndWeb;
   } else if (Platform.isAndroid) {
     return _defaultAccountServerAddressAndroid;
   } else {
