ef8e3590035ae574dce8b77f1fd2905c80cd2597
Juuso Tuononen <jutuon@outlook.com>
2025-05-09 16:07:56 +0300

    Improve server address selection screen
    Ignore change address event if the current address is the new address.
    
    Prevent changing to server which does not support backend version API
    request.

--- lib/logic/server/address.dart
+++ lib/logic/server/address.dart
@@ -1,3 +1,5 @@
+import "package:app/api/api_provider.dart";
+import "package:app/ui_utils/snack_bar.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:app/config.dart";
 import "package:app/data/login_repository.dart";
@@ -13,7 +15,25 @@ class ServerAddressBloc extends Bloc<ServerAddressEvent, String> {
 
   ServerAddressBloc() : super(defaultServerUrlAccount()) {
     on<ChangeCachedServerAddress>((data, emit) async {
+      if (data.value == state) {
+        return;
+      }
+
+      const error = "Error: server address change failed, Details: backend version request failed";
+      try {
+        final api = ApiProvider(data.value);
+        final value = await api.common.getVersion();
+        if (value == null) {
+          showSnackBar(error);
+          return;
+        }
+      } catch (_) {
+        showSnackBar(error);
+        return;
+      }
+
       await login.setCurrentServerAddress(data.value);
+      showSnackBar("Server address changed");
       emit(data.value);
     });
 
