2bb8e95eeb6b8448cb14d97acc9ddb697824a1d9
Juuso Tuononen <jutuon@outlook.com>
2025-06-01 19:57:58 +0300

    Add GenericProfileGrid

--- lib/ui/normal/chat/select_match.dart
+++ lib/ui/normal/chat/select_match.dart
@@ -1,137 +1,27 @@
 import 'dart:async';
 
-import 'package:app/logic/profile/view_profiles.dart';
-import 'package:app/logic/settings/ui_settings.dart';
-import 'package:app/model/freezed/logic/profile/view_profiles.dart';
-import 'package:app/model/freezed/logic/settings/ui_settings.dart';
-import 'package:app/ui_utils/extensions/other.dart';
+import 'package:app/ui_utils/profile_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logging/logging.dart';
-import 'package:openapi/api.dart';
 import 'package:app/data/chat/matches_iterator_manager.dart';
-import 'package:app/data/chat_repository.dart';
 import 'package:app/data/login_repository.dart';
-import 'package:app/data/profile_repository.dart';
 import 'package:database/database.dart';
-import 'package:app/database/account_database_manager.dart';
 import 'package:app/logic/app/navigator_state.dart';
-import 'package:app/logic/profile/my_profile.dart';
-import 'package:app/model/freezed/logic/main/navigator_state.dart';
-import 'package:app/model/freezed/logic/profile/my_profile.dart';
-import 'package:app/ui/normal/profiles/profile_grid.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
 import 'package:app/localizations.dart';
-import 'package:app/ui_utils/list.dart';
-import 'package:app/utils/result.dart';
 
 final log = Logger("SelectMatchScreen");
 
 Future<ProfileEntry?> openSelectMatchView(
   BuildContext context,
 ) {
-  final pageKey = PageKey();
-  return MyNavigator.pushWithKey(
+  return MyNavigator.push(
     context,
-    MaterialPage<ProfileEntry>(child: SelectMatchScreen(pageKey: pageKey)),
-    pageKey,
+    const MaterialPage<ProfileEntry>(child: SelectMatchScreen()),
   );
 }
 
-class SelectMatchScreen extends StatefulWidget {
-  final PageKey pageKey;
-  const SelectMatchScreen({
-    required this.pageKey,
-    super.key,
-  });
-
-  @override
-  State<SelectMatchScreen> createState() => SelectMatchScreenState();
-}
-
-typedef MatchViewEntry = ({ProfileEntry profile, ProfileActionState? initialProfileAction});
-
-class SelectMatchScreenState extends State<SelectMatchScreen> {
-  final ScrollController _scrollController = ScrollController();
-  PagingController<int, MatchViewEntry>? _pagingController =
-    PagingController(firstPageKey: 0);
-  StreamSubscription<ProfileChange>? _profileChangesSubscription;
-
-  final ChatRepository chat = LoginRepository.getInstance().repositories.chat;
-  final ProfileRepository profile = LoginRepository.getInstance().repositories.profile;
-
-  final AccountDatabaseManager accountDb = LoginRepository.getInstance().repositories.accountDb;
-
-  final MatchesIteratorManager _mainProfilesViewIterator = MatchesIteratorManager(
-    LoginRepository.getInstance().repositories.chat,
-    LoginRepository.getInstance().repositories.media,
-    LoginRepository.getInstance().repositories.accountBackgroundDb,
-    LoginRepository.getInstance().repositories.accountDb,
-    LoginRepository.getInstance().repositories.connectionManager,
-    LoginRepository.getInstance().repositories.chat.currentUser,
-  );
-  bool _reloadInProgress = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _mainProfilesViewIterator.reset(true);
-
-    _pagingController?.addPageRequestListener((pageKey) {
-      _fetchPage(pageKey);
-    });
-    _profileChangesSubscription?.cancel();
-    _profileChangesSubscription = profile.profileChanges.listen((event) {
-      _handleProfileChange(event);
-    });
-  }
-
-  void _handleProfileChange(ProfileChange event) {
-    switch (event) {
-      case ProfileBlocked():
-        _removeAccountIdFromList(event.profile);
-      case ProfileNowPrivate() ||
-        ProfileUnblocked() ||
-        ConversationChanged() ||
-        ReloadMainProfileView() ||
-        ProfileFavoriteStatusChange(): {}
-    }
-  }
-
-  void _removeAccountIdFromList(AccountId accountId) {
-    final controller = _pagingController;
-    if (controller != null) {
-      setState(() {
-        controller.itemList?.removeWhere((item) => item.profile.uuid == accountId);
-      });
-    }
-  }
-
-  Future<void> _fetchPage(int pageKey) async {
-    if (pageKey == 0) {
-      _mainProfilesViewIterator.resetToBeginning();
-    }
-
-    final profileList = await _mainProfilesViewIterator.nextList().ok();
-    if (profileList == null) {
-      // Show error UI
-      _pagingController?.error = true;
-      return;
-    }
-
-    final newList = List<MatchViewEntry>.empty(growable: true);
-    for (final profile in profileList) {
-      final initialProfileAction = await resolveProfileAction(chat, profile.uuid);
-      newList.add((profile: profile, initialProfileAction: initialProfileAction));
-    }
-
-    if (profileList.isEmpty) {
-      _pagingController?.appendLastPage([]);
-    } else {
-      _pagingController?.appendPage(newList, pageKey + 1);
-    }
-  }
+class SelectMatchScreen extends StatelessWidget {
+  const SelectMatchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -139,122 +29,19 @@ class SelectMatchScreenState extends State<SelectMatchScreen> {
       appBar: AppBar(
         title: Text(context.strings.select_match_screen_title),
       ),
-      body: content(),
-    );
-  }
-
-  Widget content() {
-    return RefreshIndicator(
-      onRefresh: () async {
-        if (!_reloadInProgress) {
-          await refreshProfileGrid();
-        }
-      },
-      child: Column(children: [
-        Expanded(
-          child: BlocBuilder<UiSettingsBloc, UiSettingsData>(
-            buildWhen: (previous, current) => previous.gridSettings != current.gridSettings,
-            builder: (context, uiSettings) {
-              return BlocBuilder<MyProfileBloc, MyProfileData>(
-                builder: (context, myProfileState) {
-                  return grid(context, myProfileState.profile?.unlimitedLikes ?? false, uiSettings.gridSettings);
-                },
-              );
-            }
-          ),
+      body: GenericProfileGrid(
+          buildIteratorManager: () {
+            return MatchesIteratorManager(
+              LoginRepository.getInstance().repositories.chat,
+              LoginRepository.getInstance().repositories.media,
+              LoginRepository.getInstance().repositories.accountBackgroundDb,
+              LoginRepository.getInstance().repositories.accountDb,
+              LoginRepository.getInstance().repositories.connectionManager,
+              LoginRepository.getInstance().repositories.chat.currentUser,
+            );
+          },
+          noProfilesText: context.strings.chat_list_screen_no_matches_found,
         ),
-      ],),
     );
-  }
-
-  Widget grid(BuildContext context, bool iHaveUnlimitedLikesEnabled, GridSettings settings) {
-    return PagedGridView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      scrollController: _scrollController,
-      pagingController: _pagingController!,
-      padding: EdgeInsets.symmetric(horizontal: settings.valueHorizontalPadding()),
-      builderDelegate: PagedChildBuilderDelegate<MatchViewEntry>(
-        animateTransitions: true,
-        itemBuilder: (context, item, index) {
-          return GestureDetector(
-              child: profileEntryWidgetStream(
-                item.profile,
-                iHaveUnlimitedLikesEnabled,
-                item.initialProfileAction,
-                accountDb,
-                settings,
-                overrideOnTap: (context) {
-                  MyNavigator.removePage(context, widget.pageKey, item.profile);
-                },
-            )
-          );
-        },
-        noItemsFoundIndicatorBuilder: (context) {
-          return buildListReplacementMessage(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  context.strings.chat_list_screen_no_matches_found,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ],
-            ),
-          );
-        },
-        firstPageErrorIndicatorBuilder: (context) {
-          return errorDetectedWidgetWithRetryButton(context);
-        },
-        newPageErrorIndicatorBuilder: (context) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(context.strings.generic_profile_loading_failed),
-            ),
-          );
-        },
-      ),
-      gridDelegate: settings.toSliverGridDelegate(),
-    );
-  }
-
-  Widget errorDetectedWidgetWithRetryButton(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const Spacer(),
-          Text(context.strings.generic_profile_loading_failed),
-          const Padding(padding: EdgeInsets.all(8)),
-          ElevatedButton(
-            onPressed: () {
-              if (!_reloadInProgress) {
-                refreshProfileGrid();
-              }
-            },
-            child: Text(context.strings.generic_try_again),
-          ),
-          const Spacer(flex: 3),
-        ],
-      ),
-    );
-  }
-
-  Future<void> refreshProfileGrid() async {
-    _reloadInProgress = true;
-    await _mainProfilesViewIterator.loadingInProgress.firstWhere((e) => e == false);
-    _mainProfilesViewIterator.refresh();
-    // This might be disposed after resetProfileIterator completes.
-    _pagingController?.refresh();
-    _reloadInProgress = false;
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    _pagingController?.dispose();
-    _pagingController = null;
-    _profileChangesSubscription?.cancel();
-    _profileChangesSubscription = null;
-    super.dispose();
   }
 }
--- lib/ui/normal/settings/notifications/automatic_profile_search_results.dart
+++ lib/ui/normal/settings/notifications/automatic_profile_search_results.dart
@@ -1,31 +1,13 @@
 import 'dart:async';
 
 import 'package:app/data/profile/automatic_profile_search/automatic_profile_search_iterator_manager.dart';
-import 'package:app/logic/profile/view_profiles.dart';
-import 'package:app/logic/settings/ui_settings.dart';
 import 'package:app/model/freezed/logic/main/navigator_state.dart';
-import 'package:app/model/freezed/logic/profile/view_profiles.dart';
-import 'package:app/model/freezed/logic/settings/ui_settings.dart';
 import 'package:app/ui/normal/profiles.dart';
-import 'package:app/ui_utils/extensions/other.dart';
+import 'package:app/ui_utils/profile_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:openapi/api.dart';
-import 'package:app/data/chat_repository.dart';
 import 'package:app/data/login_repository.dart';
-import 'package:app/data/profile_repository.dart';
-import 'package:database/database.dart';
-import 'package:app/database/account_database_manager.dart';
 import 'package:app/logic/app/navigator_state.dart';
-import 'package:app/logic/profile/my_profile.dart';
-import 'package:app/model/freezed/logic/profile/my_profile.dart';
-import 'package:app/ui/normal/profiles/profile_grid.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
 import 'package:app/localizations.dart';
-import 'package:app/ui_utils/consts/padding.dart';
-import 'package:app/ui_utils/list.dart';
-import 'package:app/utils/result.dart';
 
 Future<void> openAutomaticProfileSearchResultsScreen(
   BuildContext context,
@@ -62,215 +44,21 @@ class AutomaticProfileSearchResultsScreenState extends State<AutomaticProfileSea
       appBar: AppBar(
         title: Text(context.strings.automatic_profile_search_results_screen_title),
       ),
-      body: const PublicProfileViewingBlocker(child: AutomaticProfileSearchResultsGrid()),
-    );
-  }
-}
-
-typedef AutomaticProfileSearchProfileEntry = ({ProfileEntry profile, ProfileActionState? initialProfileAction});
-
-class AutomaticProfileSearchResultsGrid extends StatefulWidget {
-  const AutomaticProfileSearchResultsGrid({super.key});
-
-  @override
-  State<AutomaticProfileSearchResultsGrid> createState() => _AutomaticProfileSearchResultsGridState();
-}
-
-class _AutomaticProfileSearchResultsGridState extends State<AutomaticProfileSearchResultsGrid> {
-  final ScrollController _scrollController = ScrollController();
-  PagingController<int, AutomaticProfileSearchProfileEntry>? _pagingController =
-    PagingController(firstPageKey: 0);
-  StreamSubscription<ProfileChange>? _profileChangesSubscription;
-
-  final ChatRepository chat = LoginRepository.getInstance().repositories.chat;
-  final ProfileRepository profile = LoginRepository.getInstance().repositories.profile;
-
-  final AccountDatabaseManager accountDb = LoginRepository.getInstance().repositories.accountDb;
-
-  final AutomaticProfileSearchIteratorManager _mainProfilesViewIterator = AutomaticProfileSearchIteratorManager(
-    LoginRepository.getInstance().repositories.chat,
-    LoginRepository.getInstance().repositories.media,
-    LoginRepository.getInstance().repositories.accountBackgroundDb,
-    LoginRepository.getInstance().repositories.accountDb,
-    LoginRepository.getInstance().repositories.connectionManager,
-    LoginRepository.getInstance().repositories.chat.currentUser,
-  );
-  bool _reloadInProgress = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _mainProfilesViewIterator.reset(true);
-
-    _pagingController?.addPageRequestListener((pageKey) {
-      _fetchPage(pageKey);
-    });
-    _profileChangesSubscription?.cancel();
-    _profileChangesSubscription = profile.profileChanges.listen((event) {
-      _handleProfileChange(event);
-    });
-  }
-
-  void _handleProfileChange(ProfileChange event) {
-    switch (event) {
-      case ProfileBlocked():
-        _removeAccountIdFromList(event.profile);
-      case ProfileNowPrivate() ||
-        ProfileUnblocked() ||
-        ConversationChanged() ||
-        ReloadMainProfileView() ||
-        ProfileFavoriteStatusChange(): {}
-    }
-  }
-
-  void _removeAccountIdFromList(AccountId accountId) {
-    final controller = _pagingController;
-    if (controller != null) {
-      setState(() {
-        controller.itemList?.removeWhere((item) => item.profile.uuid == accountId);
-      });
-    }
-  }
-
-  Future<void> _fetchPage(int pageKey) async {
-    if (pageKey == 0) {
-      _mainProfilesViewIterator.resetToBeginning();
-    }
-
-    final profileList = await _mainProfilesViewIterator.nextList().ok();
-    if (profileList == null) {
-      // Show error UI
-      _pagingController?.error = true;
-      return;
-    }
-
-    final newList = List<AutomaticProfileSearchProfileEntry>.empty(growable: true);
-    for (final profile in profileList) {
-      final initialProfileAction = await resolveProfileAction(chat, profile.uuid);
-      newList.add((profile: profile, initialProfileAction: initialProfileAction));
-    }
-
-    if (profileList.isEmpty) {
-      _pagingController?.appendLastPage([]);
-    } else {
-      _pagingController?.appendPage(newList, pageKey + 1);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return content();
-  }
-
-  Widget content() {
-    return RefreshIndicator(
-      onRefresh: () async {
-        if (!_reloadInProgress) {
-          await refreshProfileGrid();
-        }
-      },
-      child: Column(children: [
-        Expanded(
-          child: BlocBuilder<UiSettingsBloc, UiSettingsData>(
-            buildWhen: (previous, current) => previous.gridSettings != current.gridSettings,
-            builder: (context, uiSettings) {
-              return BlocBuilder<MyProfileBloc, MyProfileData>(
-                builder: (context, myProfileState) {
-                  return grid(context, myProfileState.profile?.unlimitedLikes ?? false, uiSettings.gridSettings);
-                },
-              );
-            }
-          ),
+      body: PublicProfileViewingBlocker(
+        child: GenericProfileGrid(
+          buildIteratorManager: () {
+            return AutomaticProfileSearchIteratorManager(
+              LoginRepository.getInstance().repositories.chat,
+              LoginRepository.getInstance().repositories.media,
+              LoginRepository.getInstance().repositories.accountBackgroundDb,
+              LoginRepository.getInstance().repositories.accountDb,
+              LoginRepository.getInstance().repositories.connectionManager,
+              LoginRepository.getInstance().repositories.chat.currentUser,
+            );
+          },
+          noProfilesText: context.strings.automatic_profile_search_results_screen_no_profiles_found,
         ),
-      ],),
-    );
-  }
-
-  Widget grid(BuildContext context, bool iHaveUnlimitedLikesEnabled, GridSettings settings) {
-    return PagedGridView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      scrollController: _scrollController,
-      pagingController: _pagingController!,
-      padding: const EdgeInsets.symmetric(horizontal: COMMON_SCREEN_EDGE_PADDING),
-      builderDelegate: PagedChildBuilderDelegate<AutomaticProfileSearchProfileEntry>(
-        animateTransitions: true,
-        itemBuilder: (context, item, index) {
-          return GestureDetector(
-              child: profileEntryWidgetStream(
-                item.profile,
-                iHaveUnlimitedLikesEnabled,
-                item.initialProfileAction,
-                accountDb,
-                settings,
-            )
-          );
-        },
-        noItemsFoundIndicatorBuilder: (context) {
-          return buildListReplacementMessage(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  context.strings.automatic_profile_search_results_screen_no_profiles_found,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ],
-            ),
-          );
-        },
-        firstPageErrorIndicatorBuilder: (context) {
-          return errorDetectedWidgetWithRetryButton(context);
-        },
-        newPageErrorIndicatorBuilder: (context) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(context.strings.generic_profile_loading_failed),
-            ),
-          );
-        },
-      ),
-      gridDelegate: settings.toSliverGridDelegate(),
-    );
-  }
-
-  Widget errorDetectedWidgetWithRetryButton(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const Spacer(),
-          Text(context.strings.generic_profile_loading_failed),
-          const Padding(padding: EdgeInsets.all(8)),
-          ElevatedButton(
-            onPressed: () {
-              if (!_reloadInProgress) {
-                refreshProfileGrid();
-              }
-            },
-            child: Text(context.strings.generic_try_again),
-          ),
-          const Spacer(flex: 3),
-        ],
       ),
     );
-  }
-
-  Future<void> refreshProfileGrid() async {
-    _reloadInProgress = true;
-    await _mainProfilesViewIterator.loadingInProgress.firstWhere((e) => e == false);
-    _mainProfilesViewIterator.refresh();
-    // This might be disposed after resetProfileIterator completes.
-    _pagingController?.refresh();
-    _reloadInProgress = false;
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    _pagingController?.dispose();
-    _pagingController = null;
-    _profileChangesSubscription?.cancel();
-    _profileChangesSubscription = null;
-    super.dispose();
   }
 }
