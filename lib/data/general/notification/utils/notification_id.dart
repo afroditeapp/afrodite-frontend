e017fcc28f41e857886fa2827ff6aff9571178b6
Juuso Tuononen <jutuon@outlook.com>
2025-09-27 17:48:14 +0300

    Update notification IDs

--- lib/data/general/notification/utils/notification_id.dart
+++ lib/data/general/notification/utils/notification_id.dart
@@ -6,29 +6,30 @@ class LocalNotificationId {
 }
 
 enum NotificationIdStatic {
-  /// Server uses this as FCM collapse ID for data messages.
-  empty(id: LocalNotificationId(0)),
+  notificationDecryptingFailed(id: LocalNotificationId(0)),
 
-  /// Server sends this FCM visible notification if client does not respond
-  /// to the data message.
-  newNotificationAvailable(id: LocalNotificationId(1)),
-
-  // Notifications which client shows
-
-  likeReceived(id: LocalNotificationId(2)),
-  mediaContentModerationAccepted(id: LocalNotificationId(3)),
-  mediaContentModerationRejected(id: LocalNotificationId(4)),
-  mediaContentModerationDeleted(id: LocalNotificationId(5)),
-  newsItemAvailable(id: LocalNotificationId(6)),
-  profileNameModerationAccepted(id: LocalNotificationId(7)),
-  profileNameModerationRejected(id: LocalNotificationId(8)),
-  profileTextModerationAccepted(id: LocalNotificationId(9)),
-  profileTextModerationRejected(id: LocalNotificationId(10)),
-  automaticProfileSearchCompleted(id: LocalNotificationId(11)),
-
+  // Common
   /// Category: NotificationCategoryNewsItemAvailable
-  adminNotification(id: LocalNotificationId(12)),
-  genericMessageReceived(id: LocalNotificationId(13)),
+  adminNotification(id: LocalNotificationId(10)),
+
+  // Account
+  newsItemAvailable(id: LocalNotificationId(20)),
+
+  // Profile
+  profileNameModerationAccepted(id: LocalNotificationId(30)),
+  profileNameModerationRejected(id: LocalNotificationId(31)),
+  profileTextModerationAccepted(id: LocalNotificationId(32)),
+  profileTextModerationRejected(id: LocalNotificationId(33)),
+  automaticProfileSearchCompleted(id: LocalNotificationId(34)),
+
+  // Media
+  mediaContentModerationAccepted(id: LocalNotificationId(40)),
+  mediaContentModerationRejected(id: LocalNotificationId(41)),
+  mediaContentModerationDeleted(id: LocalNotificationId(42)),
+
+  // Chat
+  likeReceived(id: LocalNotificationId(50)),
+  genericMessageReceived(id: LocalNotificationId(51)),
   firstNewMessageNotificationId(id: LocalNotificationId(1000));
 
   final LocalNotificationId id;
