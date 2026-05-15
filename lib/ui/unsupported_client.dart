68eaa675264e6df49c7957b5d6fafced30e27a34
Juuso Tuononen <jutuon@outlook.com>
2026-05-15 13:41:57 +0300

    Add update button to UnsupportedClientScreen

--- lib/ui/unsupported_client.dart
+++ lib/ui/unsupported_client.dart
@@ -1,8 +1,10 @@
 import "package:app/localizations.dart";
 import "package:app/model/freezed/logic/main/navigator_state.dart";
+import "package:app/ui/utils/open_app_store.dart";
 import "package:app/ui_utils/app_bar/common_actions.dart";
 import "package:app/ui_utils/app_bar/menu_actions.dart";
 import "package:app/ui_utils/list.dart";
+import "package:app/ui_utils/snack_bar.dart";
 import "package:flutter/material.dart";
 
 class UnsupportedClientPage extends MyScreenPage<()> with SimpleUrlParser<UnsupportedClientPage> {
@@ -14,6 +16,13 @@ class UnsupportedClientPage extends MyScreenPage<()> with SimpleUrlParser<Unsupp
 
 class UnsupportedClientScreen extends StatelessWidget {
   const UnsupportedClientScreen({super.key});
+
+  Future<void> updateApp(BuildContext context) async {
+    final launchSuccessful = await openStorePageForCurrentApp();
+    if (!launchSuccessful && context.mounted) {
+      showSnackBar(context.strings.generic_error_occurred);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +38,22 @@ class UnsupportedClientScreen extends StatelessWidget {
   }
 
   Widget showInfo(BuildContext context) {
+    final showUpdateButton = isDirectUpdateActionSupportedForCurrentPlatform();
+
     return buildListReplacementMessage(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
-        children: [Text(context.strings.unsupported_client_screen_info)],
+        children: [
+          Text(context.strings.unsupported_client_screen_info),
+          if (showUpdateButton) ...[
+            const SizedBox(height: 16),
+            FilledButton(
+              onPressed: () => updateApp(context),
+              child: Text(context.strings.generic_update),
+            ),
+          ],
+        ],
       ),
     );
   }
