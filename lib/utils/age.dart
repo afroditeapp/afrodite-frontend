d6be803ea734b9555cfe28985d4175c919f33697
Juuso Tuononen <jutuon@outlook.com>
2026-05-23 14:17:18 +0300

    Remove unused ageInYearsFromBirthdate function

--- lib/utils/age.dart
+++ lib/utils/age.dart
@@ -8,14 +8,3 @@ bool ageIsValid(int? age) {
 bool ageRangeIsValid(int? min, int? max) {
   return min != null && max != null && min <= max && ageIsValid(min) && ageIsValid(max);
 }
-
-int ageInYearsFromBirthdate(DateTime birthdate) {
-  final now = DateTime.now();
-  if (now.month < birthdate.month || now.month == birthdate.month && now.day < birthdate.day) {
-    // Before birthday
-    return now.year - birthdate.year - 1;
-  } else {
-    // Birthday or after birthday
-    return now.year - birthdate.year;
-  }
-}
